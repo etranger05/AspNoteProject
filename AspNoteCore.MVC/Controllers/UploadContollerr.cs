bc3a3ab27589091d8d90a6d3bc9628298ac13231
etranger05 <etranger05@gmail.com>
2020-06-23 15:28:20 +0100

    Add WYSIWIG Editor to note page Edit detail page to show HTML

---
+++ AspNoteCore.MVC/Controllers/UploadContollerr.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNoteCore.MVC.Controllers
+{
+    public class UploadContollerr : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
